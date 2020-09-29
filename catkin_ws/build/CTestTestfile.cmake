9e4e6d964d62360b9577e5fbcff5c2e591285a5f
sss <ssschou@outlook.dk>
2020-09-29 11:13:54 +0200

    Fixed untracked files

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sebastian/Master/git/FloorDetectionProject/catkin_ws/src
-# Build directory: /home/sebastian/Master/git/FloorDetectionProject/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("master_project")
