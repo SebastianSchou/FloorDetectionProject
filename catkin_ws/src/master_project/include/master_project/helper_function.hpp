f6664770f921942a280e87e2759c7ee36dd8a21b
sss <ssschou@outlook.dk>
2020-10-02 14:51:42 +0200

    Updated helper_function

--- catkin_ws/src/master_project/include/master_project/helper_function.hpp
+++ catkin_ws/src/master_project/include/master_project/helper_function.hpp
@@ -1,7 +1,10 @@
-class HelperFunction{
-public:
-	HelperFunction();
-	~HelperFunction();
+#ifndef HELPER_FUNCTION_HPP
+#define HELPER_FUNCTION_HPP
 
+#include <chrono>
+
+namespace HelperFunction {
 	float msUntilNow(const std::chrono::steady_clock::time_point& start);
 };
+
+#endif // HELPER_FUNCTION_HPP
--- catkin_ws/src/master_project/src/helper_function.cpp
+++ catkin_ws/src/master_project/src/helper_function.cpp
@@ -1,7 +1,6 @@
-#include <chrono>
+#include "master_project/helper_function.hpp"
 
-
-float msUntilNow(const std::chrono::steady_clock::time_point& start)
+float HelperFunction::msUntilNow(const std::chrono::steady_clock::time_point& start)
 {
     return std::chrono::duration_cast<std::chrono::microseconds>(
         std::chrono::steady_clock::now() - start).count() / 1000.0;
