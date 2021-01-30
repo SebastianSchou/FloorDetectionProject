ec806a4db6c61aa6d307c6547b1cc3c56149bb9a
sss <ssschou@outlook.dk>
2021-01-30 19:45:53 +0100

    Removed unused functions in helper_function

--- catkin_ws/src/traversable_area_detection/include/traversable_area_detection/helper_function.hpp
+++ catkin_ws/src/traversable_area_detection/include/traversable_area_detection/helper_function.hpp
@@ -9,23 +9,7 @@ static const double SQRTOFCUBIC2PI = 2.0 * sqrt(2.0) * pow(CV_PI, 1.5);
 const double NONZERO = 0.00001;
 
 float   msUntilNow(const std::chrono::steady_clock::time_point& start);
-cv::Mat normalizeVector(const cv::Mat& v);
 double  square(const double v);
-bool    isMatEqual(const cv::Mat m1, const cv::Mat m2);
-double  squareNorm(const cv::Mat& v);
 double  roundToNearestValue(const double v, const double value);
-
-class Incrementer {
-public:
-
-  Incrementer(const float min, const float max, const float step);
-  float increment();
-  float getValue();
-
-private:
-
-  float min_, max_, step_, value_;
-  int increase_;
-};
 
 #endif // HELPER_FUNCTION_HPP
